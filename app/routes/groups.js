dd7f8175b77fef9996b87216df628a8f7530a65f
David Vargas <123vargasdavid@gmail.com>
2025-12-09 15:16:50 +0100

    feat(counter): list groups implemented

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -1,3 +1,25 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class GroupsRoute extends Route {}
+export default class GroupsRoute extends Route {
+  @service groups;
+  @service session;
+
+  async model() {
+    try {
+      const groupsList = await this.groups.getGroups();
+      console.log('Groups list:', groupsList);
+
+      return {
+        groups: groupsList,
+        error: null,
+      };
+    } catch (error) {
+      console.error('Error loading data for groups route:', error);
+      return {
+        groups: [],
+        error: "The groups or user info couldn't be loaded.",
+      };
+    }
+  }
+}
